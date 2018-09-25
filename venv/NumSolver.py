214a0d6e9958ce71982d8ea096eb7f920abfff47
Colin G <cgoodman3544@gmail.com>
2018-09-25 10:58:26 -0500

    numsolver update

--- venv/NumSolver.py
+++ venv/NumSolver.py
@@ -9,3 +9,27 @@ auth = tweepy.OAuthHandler('0oG3gXlR17hHDfrmK5ICkR6nV', '2oOe6SxCaBgNz7PTtwriSTV
 auth.set_access_token('247468435-MM16zZOkuNPIL7qE7aNRfWDs3YRx3VSmV0xWKSed', '5Bx6AbxFUdv6zh0xMU9BAwKhBuhOq0fR8inNme6MK7bVx')
 api = tweepy.API(auth)
 
+file = open('tweets.txt', 'r', encoding="utf8")
+writeTo = open('userIDs.txt', 'w', encoding="utf8")
+
+users = []  # list of user IDS
+
+loop = True
+
+while loop:
+    file.readline()  # time
+    link = file.readline().rstrip()  # user
+    file.readline()  # tweet
+    link = link.replace('U\thttp://twitter.com/', '')
+
+    if len(link) == 0:  # if end of text file, stop
+        loop = False
+    else:
+        if len(link) == 0:  # if there is nothing in the list
+            continue
+        check = False
+        for id in users:
+            if id == link:
+                check = True
+        if check == False:
+            users.Add(link)
