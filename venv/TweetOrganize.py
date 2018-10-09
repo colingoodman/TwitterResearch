ced94e5e94a292663e42df2356c2167dce808e22
Colin G <cgoodman3544@gmail.com>
2018-10-09 12:23:16 -0500

    updated comments

--- venv/TweetOrganize.py
+++ venv/TweetOrganize.py
@@ -10,6 +10,7 @@ auth = tweepy.OAuthHandler('0oG3gXlR17hHDfrmK5ICkR6nV', '2oOe6SxCaBgNz7PTtwriSTV
 auth.set_access_token('247468435-MM16zZOkuNPIL7qE7aNRfWDs3YRx3VSmV0xWKSed', '5Bx6AbxFUdv6zh0xMU9BAwKhBuhOq0fR8inNme6MK7bVx')
 api = tweepy.API(auth)
 
+# make sure these files exist
 file = open('tweets.txt', 'r', encoding="utf8")
 writeTo = open('userIDs.txt', 'w', encoding="utf8")
 
@@ -17,7 +18,7 @@ users = []  # list of tuples of users (username, user ID)
 
 loop = True
 
-while loop:
+while loop:  # build list
     file.readline()  # time
     link = file.readline().rstrip()  # user
     file.readline()  # tweet
