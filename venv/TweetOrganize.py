fc2fa5a2f7754f6e37d91fc816bd6b54c45e593f
Colin G <cgoodman3544@gmail.com>
2018-10-19 21:37:21 -0500

    Update TweetOrganize.py

--- venv/TweetOrganize.py
+++ venv/TweetOrganize.py
@@ -7,8 +7,7 @@ import os
 import tweepy
 
 # tweepy stuff
-auth = tweepy.OAuthHandler('0oG3gXlR17hHDfrmK5ICkR6nV', '2oOe6SxCaBgNz7PTtwriSTVj5Vx6lkZdoAkb4nM0BpEtZl0BOz')
-auth.set_access_token('247468435-MM16zZOkuNPIL7qE7aNRfWDs3YRx3VSmV0xWKSed', '5Bx6AbxFUdv6zh0xMU9BAwKhBuhOq0fR8inNme6MK7bVx')
+
 api = tweepy.API(auth)
 
 targetTweets = 'testTweets.txt'  # file we are opening
